e27247282021c57cfdd18a521fc5da7c0f5d8146
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2017-10-02 13:11:15 +0100

    Add clickable grid

--- src/index.js
+++ src/index.js
@@ -2,23 +2,71 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const Box = ({ selectBox, row, col, boxClass, boxId }) => {
+  const selectThisBox = () => selectBox(row, col);
+  return <div className={boxClass} id={boxId} onClick={selectThisBox} />;
+};
+
 class Grid extends React.Component {
   render() {
-    return <div>Grid</div>;
+    const width = this.props.cols * 16;
+
+    const rowsArr = this.props.gridFull.map((row, i) =>
+      row.map((box, j) => {
+        const boxId = `${i}_${j}`;
+        const boxClass = box ? "box on" : "box off";
+        return (
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        );
+      })
+    );
+
+    return (
+      <div className="grid" style={{ width: width }}>
+        {rowsArr}
+      </div>
+    );
   }
 }
 class Main extends React.Component {
   constructor() {
     super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
     this.state = {
-      generation: 0
+      generation: 0,
+      gridFull: Array(this.rows)
+        .fill()
+        .map(() => Array(this.cols).fill(false))
     };
   }
+
+  selectBox = (row, col) =>
+    this.setState(({ gridFull }) => {
+      const newVal = !gridFull[row][col];
+      gridFull[row][col] = newVal;
+      return { gridFull };
+    });
+
   render() {
     return (
       <div>
         <h1>The Game of Life</h1>
-        <Grid />
+        <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
+        />
         <h2>Generations: {this.state.generation}</h2>
       </div>
     );
